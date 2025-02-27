e241fa6f13040c893eb4087c59939013fc961957
Gur@1248 <76244882+siddhant7701@users.noreply.github.com>
2025-02-27 13:16:48 +0530

    Initial commit for SoniMensWear

---
+++ script.js
@@ -0,0 +1,13 @@
+// Simple smooth scroll for navigation links
+document.querySelectorAll("nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
+s;
